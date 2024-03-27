eb1641932d6a18e7ee288407cdc99f40594b741a
cuppajoeman <ccn@cuppajoeman.com>
2024-03-27 00:55:09 -0400

    add in probability content and improve search functionality

--- html/searching/generate_search_data.py
+++ html/searching/generate_search_data.py
@@ -18,6 +18,8 @@ files_to_ignore = ["index.html"]
 
 search_data_file = "search_data.js"
 
+types_of_knowledge = ['definition', 'theorem', 'proposition', 'lemma', 'corollary', 'exercise']
+
 search_data_file_path = f"{script_directory}/{search_data_file}"
 
 def path_to_be_ignored(dir_path: str) -> bool:
@@ -67,44 +69,45 @@ class Definition:
 
 
 def extract_search_results(content_file_paths: List[str]) -> Tuple[List[str], Dict[str, str]]:
-    all_definitions = []
-    definition_id_to_relative_path = {}
-    all_definition_ids = []
+    all_knowledges = []
+    knowledge_id_to_relative_path = {}
+    all_knowledge_ids = []
 
     for content_file_path in content_file_paths:
         html_file_contents = open(content_file_path).read()
         parsed_html = BeautifulSoup(html_file_contents, 'html.parser')
 
-        all_definitions_in_file = parsed_html.find_all("div", class_="definition")
+        # all_knowledges_in_file = parsed_html.find_all("div", class_="knowledge")
+        all_knowledge_in_file = parsed_html.find_all(True, {'class': types_of_knowledge})
 
-        for definition in all_definitions_in_file:
-            id = definition.get('id')
-            all_definition_ids.append(id)
+        for knowledge in all_knowledge_in_file:
+            id = knowledge.get('id')
+            all_knowledge_ids.append(id)
 
-            definition_id_to_relative_path[id] = content_file_path
+            knowledge_id_to_relative_path[id] = content_file_path
 
-            definition = Definition(id, content_file_path)
-            all_definitions.append(definition)
+            knowledge = Definition(id, content_file_path)
+            all_knowledges.append(knowledge)
 
-    return all_definition_ids, definition_id_to_relative_path
+    return all_knowledge_ids, knowledge_id_to_relative_path
 
-def write_search_data_to_file(all_definition_ids, definition_id_to_relative_path: Dict[str, str]):
+def write_search_data_to_file(all_knowledge_ids, knowledge_id_to_relative_path: Dict[str, str]):
     f = open(search_data_file_path, "w")
 
     f.write("var knowledge_ids = [\n")
 
-    for definition_id in all_definition_ids:
-        if definition_id is not None:
-            f.write(f'\t"{definition_id}",\n')
+    for knowledge_id in all_knowledge_ids:
+        if knowledge_id is not None:
+            f.write(f'\t"{knowledge_id}",\n')
 
     f.write("]\n")
 
     f.write("var knowledge_id_to_relative_path = {\n")
 
-    for (definition_id, relative_path) in definition_id_to_relative_path.items():
-        if definition_id is not None:
-            local_link_to_knowledge_id = path_delimiter + relative_path + "#" + definition_id
-            f.write(f'\t"{definition_id}": "{local_link_to_knowledge_id}",\n')
+    for (knowledge_id, relative_path) in knowledge_id_to_relative_path.items():
+        if knowledge_id is not None:
+            local_link_to_knowledge_id = path_delimiter + relative_path + "#" + knowledge_id
+            f.write(f'\t"{knowledge_id}": "{local_link_to_knowledge_id}",\n')
 
     f.write("}\n")
 
@@ -113,8 +116,8 @@ def write_search_data_to_file(all_definition_ids, definition_id_to_relative_path
 
 if __name__ == "__main__":
     content_file_paths = create_search_data()
-    all_definition_ids, definition_id_to_relative_path = extract_search_results(content_file_paths)
-    write_search_data_to_file(all_definition_ids, definition_id_to_relative_path)
+    all_knowledge_ids, knowledge_id_to_relative_path = extract_search_results(content_file_paths)
+    write_search_data_to_file(all_knowledge_ids, knowledge_id_to_relative_path)
 
     # pprint.pprint(definitions)
 
