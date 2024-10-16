d826090aaa69f37ef54222b3967432712ff4c375
cuppajoeman <ccn@cuppajoeman.com>
2024-10-15 23:44:35 -0400

    feat: add pyperclip dep

--- scripts/codespaces_setup/setup_dependencies.sh
+++ scripts/codespaces_setup/setup_dependencies.sh
@@ -2,8 +2,8 @@
 
 # Check if BeautifulSoup and latex2mathml are installed
 if ! python3 -c "import bs4, latex2mathml" &> /dev/null; then
-  echo "Installing necessary dependencies: BeautifulSoup4 and latex2mathml..."
-  pip install beautifulsoup4 latex2mathml
+  echo "Installing necessary dependencies: BeautifulSoup4, latex2mathml, pyperclip"
+  pip install beautifulsoup4 latex2mathml pyperclip
 else
   echo "Dependencies are already installed."
 fi
